d4aed542cfe4de0a2448c2c6281cbed51be2e165
matt <matthew.dobrowolski5@gmail.com>
2018-05-15 16:36:12 -0600

    ti

--- GetThyRing/DataLayer1/Interface/IPickupAble.cs
+++ GetThyRing/DataLayer1/Interface/IPickupAble.cs
@@ -1,27 +1,12 @@
 ﻿using System;
 namespace DataLayer1.Interface
 {
-    public interface IPickupAble
-    {
-	    
+	public interface IPickupAble
+	{
+
 		Action Pickup { get; set; }
-	    string PickupAble();
+        
 
 	}
-
-
-    public class Canpickup{
-        public IPickupAble c { get; set; }
-        public string TryToPickUp(){
-            c.PickupAble();
-        }
-    }
-
-
-
-
-
-
-
 }
 
---
+++ GetThyRing/DataLayer1/Items/SpecialItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace DataLayer1.Items
+{
+	public class SpecialItem
+	{
+		public int Type { get; set; }
+        public SpecialItem(int type )
+		{
+			type = 2;
+		}
+
+
+	}
+}
+           
